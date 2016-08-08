3c05f092d95a7793948e870f642b734cea4d0b1f
nikki taliaferro <hello@nikkitaliaferro.com>
2016-08-08 11:48:04 -0400

    Checkpoint 8.3 Services: complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,19 +1,17 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
           var SongPlayer = {};
 
           /**
-          * @desc current song object
-          * @type {Object}
-          **/
-          var currentSong = null;
+          * @desc Current album information
+          * @type {Object} album
+          **/ 
+          var currentAlbum = Fixtures.getAlbum();
 
           /**
           * @desc Buzz audio file object
           * @type {Object}
           **/
-
-    
           var currentBuzzObject = null;
 
           /**
@@ -25,7 +23,7 @@
           var setSong = function(song) {
               if (currentBuzzObject) {
                   currentBuzzObject.stop();
-                  currentSong.playing = null;
+                  SongPlayer.currentSong.playing = null;
               }
 
               currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -33,7 +31,7 @@
                   preload: true
               });
 
-              currentSong = song;
+              SongPlayer.currentSong = song;
           };
 
           /**
@@ -41,7 +39,6 @@
           * @desc  Plays audio file as currentBuzzObject and sets the .playing property of the object to true
           * @param {Object} song
           **/
-
           // private function
           var playSong = function(song) {
           // plays current Buzz object!
@@ -51,23 +48,69 @@
               song.playing = true;  
           };
 
+          /**
+          * @desc Return index of song
+          * @type Number
+          **/
+          var getSongIndex = function(song) {
+              return currentAlbum.songs.indexOf(song);
+          };
+
+          /**
+          * @desc Active song object from list of songs
+          * @type {Object}
+          **/
+          SongPlayer.currentSong = null;
+
+          /**
+          * @function play
+          * @desc Play current or new song
+          * @param {Object} song
+          **/
           SongPlayer.play = function(song) {
-              if (currentSong !== song) {
+              song = song || SongPlayer.currentSong;
+              if (SongPlayer.currentSong !== song) {
                  setSong(song);
           // replace two instances of song.playing w/ playSong
           // #1
                  playSong(song);
 
-              } else if (currentSong === song) {
+              } else if (SongPlayer.currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
                      playSong(song);
                  }
               }
           };
 
+          /**
+          * @function pause
+          * @desc Pause current song
+          * @param {Object} song
+          **/
           SongPlayer.pause = function(song) {
+              song = song || SongPlayer.currentSong;
               currentBuzzObject.pause();
               song.playing = false;
+          };
+
+          /**
+          * @function previous
+          * @desc Skips to song prior to current song
+          * @param {Object} song
+          **/
+          SongPlayer.previous = function() {
+              var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+              currentSongIndex--;
+          // if user is on the first song
+              if (currentSongIndex < 0) {
+                  currentBuzzObject.stop();
+                  SongPlayer.currentSong.playing = null;
+          // if user is not on first song        
+              } else {
+                  var song = currentAlbum.songs[currentSongIndex]
+                  setSong(song);
+                  playSong(song);
+              }   
           };
 
           return SongPlayer;
