22d5359f452cc694551f360d48f614b8274f0cd0
nikki taliaferro <hello@nikkitaliaferro.com>
2016-08-25 20:22:59 -0400

    Checkpoint 10.2 directives: completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -20,12 +20,23 @@
         templateUrl: '/templates/directives/seek_bar.html',
         replace: true,
         restrict: 'E',
-        scope: { },
+        scope: {
+            onChange: '&'
+        },
         link: function(scope, element, attributes) {
             scope.value = 0;
             scope.max = 100;
 
             var seekBar = $(element);
+
+
+            attributes.$observe('value', function(newValue) {
+                scope.value = newValue;
+            });
+
+            attributes.$observe('max', function(newValue) {
+                scope.max = newValue;
+            });
 
             var percentString = function() {
                 var value = scope.value;
@@ -43,20 +54,32 @@
                 return {left: percentString()};
             };
 
+            var notifyOnChange = function(newValue) {
+                if (typeof scope.onChange === 'function') {
+                    scope.onChange({value: newValue});
+                }
+            };
+
             /**
-            * @desc Updates seek bar valuse based on 
+            * @desc Updates seek bar values based on click
             * @type
             **/
             scope.onClickSeekBar = function(event) {
                 var percent = calculatePercent(seekBar, event);
                 scope.value = percent * scope.max;
+                notifyOnChange(scope.value);
             };
 
+            /**
+            * @desc Updates seek bar values based on draging thumb
+            *
+            **/
             scope.trackThumb = function() {
                 $document.bind('mousemove.thumb', function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.$apply(function() {
                         scope.value = percent * scope.max;
+                        notifyOnChange(scope.value);
                     });
 
                 });
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           var SongPlayer = {};
 
           /**
@@ -29,6 +29,12 @@
               currentBuzzObject = new buzz.sound(song.audioUrl, {
                   formats: ['mp3'],
                   preload: true
+              });
+
+              currentBuzzObject.bind('timeupdate', function() {
+                  $rootScope.$apply(function() {
+                      SongPlayer.currentTime = currentBuzzObject.getTime();
+                  });
               });
 
               SongPlayer.currentSong = song;
@@ -61,6 +67,12 @@
           * @type {Object}
           **/
           SongPlayer.currentSong = null;
+
+          /**
+          * @desc Current playback time (in seconds) of currently playing song
+          * @type {Number}
+          **/
+          SongPlayer.currentTime = null;
 
           /**
           * @function play
@@ -141,7 +153,20 @@
                   var song = currentAlbum.songs[currentSongIndex];
                   setSong(song);
                   playSong(song);
-              }    
+              }  
+
+          };
+          
+          /**
+          * @function setCurrentTime
+          * @desc Set current time (in seconds) of currently playing song
+          * @param {Number} time
+          **/
+          SongPlayer.setCurrentTime = function(time) {
+              if (currentBuzzObject) {
+                  currentBuzzObject.setTime(time);
+              }
+
           };
 
           return SongPlayer;
@@ -150,5 +175,5 @@
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
